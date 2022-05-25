38fbb38107e218abb4579ce629539e8a4a3109b7
Miguel Figuera <miguelqui725@gmail.com>
2022-05-24 20:49:55 -0400

    display good

--- lib/game.rb
+++ lib/game.rb
@@ -11,37 +11,59 @@ end
 #Game Class
 
 class Game
-    attr_accessor :word, :rounds_left, :human_guesses
+    attr_accessor :word, :rounds_left, :human_guesses_good, :human_guesses_bad, :display
     def initialize
         @word = []
         @rounds_left = 0
-        @human_guesses = []
-        @human_guesses_display[]
+        @human_guesses_good = []
+        @display[]
     end
 
     words = File.open("../repos/HangMan/10000_english_words.txt","r")
 
     def human_guesses(letter)
-        @human_guesses.push(letter)
+        if @word.includes?(letter)
+            @human_guesses_good.push(letter)
     end
 
     def rounds_left_method
         @rounds_left = (@word.length * 1.5).to_i
     end
 
+    def update_rounds
+        @rounds_left -= 1
+    end
+
+    def defeat
+        @rounds_left== 0
+    end
+
     def pushing_nils_for_view
         num = @word.length
         num.times do
-            @human_guesses.push(nil)
+            @display.push(nil)
         end
-        puts @human_guesses
+        puts @display
     end
+
+    def display_goods_and_bads
+        @display = []
+        @word.each do |letter|
+            if @human_guesses_good.include?(letter)
+                display.push(letter)
+            else
+                display.push("_")
+            end
+        end
+        puts @display
+    end
+        
 
     def count_letters(letter)
         number= @word.count(letter)
         if 0 < number
             number.times do
-                human_guesses(letter)
+                display.push(letter)
             end
         end
     end
--- lib/text_module.rb
+++ lib/text_module.rb
@@ -1,3 +1,24 @@
 module Text
     def greetings
-        puts 
+        puts <<-HEREDOC
+
+        Hello! Welcome to the HangMan
+        Rules are simple:
+        1) There is a secret word
+        2) You have an specific number of guesses.
+        3) If you don't guess, you loose.
+        HEREDOC
+    end
+
+    def whats_your_name
+        puts "What is your name?"
+    end
+
+    def really_text
+        puts "Really? Ufff... I'm sorry... well lets continue..."
+    end
+
+
+#end of module
+
+end

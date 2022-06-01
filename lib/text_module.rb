6c23c4b774de3fea53a01d69247438dc19a23a1b
Miguel Figuera <miguelqui725@gmail.com>
2022-05-31 20:23:55 -0400

    Working on saving the game after a rubocop pass

--- lib/text_module.rb
+++ lib/text_module.rb
@@ -1,6 +1,6 @@
 module Text
-    def greetings
-        puts <<-HEREDOC
+  def greetings
+    puts <<-HEREDOC
 
         Welcome to the HangMan
         Rules are simple:
@@ -8,29 +8,28 @@ module Text
         2) You have an specific number of guesses.
         3) If you don't guess, you loose.
         4) Only one letter at a time.
-        HEREDOC
-    end
+    HEREDOC
+  end
 
-    def whats_your_name_text
-        puts "What is your name?"
-    end
+  def whats_your_name_text
+    puts 'What is your name?'
+  end
 
-    def really_text
-        puts "Really? Ufff... I'm sorry... well lets continue..."
-    end
+  def really_text
+    puts "Really? Ufff... I'm sorry... well lets continue..."
+  end
 
-#method's text
-    def answer_text
-        puts "Please choose a letter or write 'save' to save your game"
-    end
+  # method's text
+  def answer_text
+    puts "Please choose a letter or write 'save' to save your game"
+  end
 
-    def final_text_method
-        if victoria
-            puts "Well done, lad!"
-        elsif defeat
-            puts "DUDE, THAT MAN DIED BECAUSE OF YOU!... SHAME ON YOU!"
-        end
+  def final_text_method
+    if victoria
+      puts 'Well done, lad!'
+    elsif defeat
+      puts 'DUDE, THAT MAN DIED BECAUSE OF YOU!... SHAME ON YOU!'
     end
-#end of module
-
+  end
+  # end of module
 end
